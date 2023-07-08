3f1c1479101b1461572b3831e48844ce64cf8edb
nabroo101 <elnabawy101@gmail.com>
2023-07-08 15:19:07 -0400

    map is showing on the live server

---
+++ static/infoMap.js
@@ -0,0 +1,8 @@
+let myMap = L.map("map", {
+    center: [5, 5],
+    zoom: 2.35
+  });
+
+  L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
+    attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+}).addTo(myMap);
