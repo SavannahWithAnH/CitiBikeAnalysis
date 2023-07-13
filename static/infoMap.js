f47dd94586265cc5f4fb3f17ed9f07658805dd75
nabroo101 <elnabawy101@gmail.com>
2023-07-13 15:54:41 -0400

    added the comany : symbol text

--- static/infoMap.js
+++ static/infoMap.js
@@ -1,3 +1,143 @@
+// // Create base layers for the map
+// let street = L.tileLayer(
+//   "https://basemap.nationalmap.gov/arcgis/rest/services/USGSTopo/MapServer/tile/{z}/{y}/{x}",
+//   {
+//     maxZoom: 20,
+//     attribution:
+//       'Tiles courtesy of the <a href="https://usgs.gov/">U.S. Geological Survey</a>' +
+//       "<br> Analyst: Mo Abou <a href=https://github.com/nabroo101> Github<a/>",
+//   }
+// );
+
+// let satellite = L.tileLayer(
+//   "https://{s}.tile.opentopomap.org/{z}/{x}/{y}.png",
+//   {
+//     attribution:
+//       'Map data: &copy; <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors' +
+//       "<br> Analyst: Mo Abou <a href=https://github.com/nabroo101/leaflet-challenge> Github<a/>",
+//   }
+// );
+
+// let carManufacturers = new L.layerGroup(); // This layer group contains all the car manufacturer icons
+
+// // Create a base map object
+// let baseMaps = {
+//   "Street Map": street,
+//   "Satellite": satellite,
+// };
+
+// let ranking = new L.layerGroup();
+
+// // Create an overlay object
+// let overlayMaps = {
+//   Ranking: ranking,
+//   "Manufacturers Info": carManufacturers,
+// };
+
+// let myMap = L.map("map", {
+//   center: [39, 4],
+//   zoom: 2,
+//   layers: [street, ranking],
+// });
+
+// function getIconSize(marketcap) {
+//   let cap = parseFloat(marketcap);
+//   let size = Math.sqrt(cap) / 1000000;
+
+//   if (size < 10) size = 10;
+//   if (size > 100) size = 100;
+
+//   return [size, size];
+// }
+
+// const largestData = "../Resources/infoMapData.json";
+
+// let timeline = L.control.timelineSlider({
+//   timelineItems: ["Before", "After"],
+//   changeMap: function ({ label, value, map }) {
+//     console.log("Changed time to:", label);
+//     d3.json(largestData).then(function (data) {
+//       let companies = reformatData(data.features);
+
+//       if (label === "After") {
+//         carManufacturers.clearLayers();
+//         companies.forEach((company) => {
+//           let customIcon = L.icon({
+//             iconUrl: company.Logo,
+//             iconSize: getIconSize(company.marketcap),
+//             iconAnchor: [19, 19],
+//             popupAnchor: [-3, -76],
+//           });
+
+//           let marker = L.marker([company.Latitude, company.Longitude], {
+//             icon: customIcon,
+//           });
+
+//           marker
+//             .bindPopup(
+//               `<h3>${company.Name}</h3>
+//               <h2>Rank: ${company.Rank}</h2>
+//               <p>Market Cap: ${company.marketcap}<br>Price (USD): ${company["price (USD)"]}</p>`
+//             )
+//             .on("click", function (e) {
+//               myMap.setView(e.latlng, 8);
+//             })
+//             .addTo(carManufacturers);
+//         });
+//       }
+//     });
+//   },
+// });
+// // converting the json file to a different format
+// function reformatData(data) {
+//   let result = [];
+
+//   for (let i = 0; i < 10; i++) {
+//     let company = {
+//       Rank: data[i].Rank,
+//       Name: data[i].Name,
+//       Symbol: data[i].Symbol,
+//       marketcap: data[i].marketcap,
+//       "price (USD)": data[i]["price (USD)"],
+//       country: data[i].country,
+//       Latitude: data[i].Latitude,
+//       Longitude: data[i].Longitude,
+//       Logo: data[i].Logo,
+//       IPO: data[i].IPO,
+//     };
+//     result.push(company);
+//   }
+
+//   return result;
+// }
+
+// timeline.addTo(myMap);
+
+
+// let originalZoom = myMap.getZoom();
+// let originalCenter = myMap.getCenter();
+
+// L.control.layers(baseMaps, overlayMaps, {
+//   collapsed: false,
+// }).addTo(myMap);
+
+// let exitButton = L.control({ position: "bottomright" });
+
+// exitButton.onAdd = function (myMap) {
+//   this._div = L.DomUtil.create("div", "exit-button");
+//   this._div.innerHTML = "Exit";
+//   this._div.onmousedown = this._div.onclick = function () {
+//     myMap.setView(originalCenter, originalZoom);
+//   };
+//   return this._div;
+// };
+
+// exitButton.addTo(myMap);
+
+// document.querySelector(".exit-button").addEventListener("click", function () {
+//   myMap.setView(originalCenter, originalZoom);
+// });
+
 //create base layer to the map
 // adding variables to the layer
 
@@ -35,44 +175,48 @@ let overlayMaps = {
   Ranking: ranking,
   "Manufacturers info": carManufacturers,
 };
-
+//my baseMap, with toggles between info and ranking
 let myMap = L.map("map", {
   center: [39, 4],
   zoom: 2,
   layers: [street, ranking],
 });
-function getIconSize(marketcap){
-  // converting marketcap from string to a number
-  if(typeof marketcap !== 'string'){
-    return[30,30]
+function getIconSize(rank, symbol) {
+  if (symbol === "TSLA") {
+    return [100, 100]; // Set a fixed size for Tesla
   }
-  let cap = Number(marketcap.replace(/[^0-9.-]+/g,""));
 
-  // setting the marketcap to a reasonable cnumber so we can adjust size
-  let size = Math.sqrt(cap)/ 1000000;
+  let size = 30; // Default size for other companies
+  let teslaSize = 100; // Size of Tesla
 
-  //setting boundries for the size
-  if (size < 20) size = 20;
-  if (size > 100) size = 100;
+  if (rank > 0 && rank <= 10) {
+    // Adjust the size for the top 10 companies
+    size = teslaSize * (11 - rank) / 10;
+  }
 
-  return [size,size]
+  return [size, size];
 }
+
 const largestData = "../Resources/infoMapData.json";
 
 //defininf the timeline variable and setting the parameters for certain years
+function markerSize(magnitude) {
+  return magnitude / 2; // Adjust the division factor as needed
+}
 let timeline = L.control.timelineSlider({
   timelineItems: ["Before", "After"],
   changeMap: function ({ label, value, map }) {
     console.log("Changed time to:", label);
     d3.json(largestData).then(function (data){
       let companies = reformatData(data);
-  
+     
+      
       if (label === "After") {
         carManufacturers.clearLayers();
         companies.forEach((company) => {
           let customIcon = L.icon({
             iconUrl: company.Logo,
-            iconSize: getIconSize(company.marketcap),
+            iconSize: getIconSize(company.marketcap, company.Symbol),
             iconAnchor: [19, 19],
             popupAnchor: [-3, -76],
           });
