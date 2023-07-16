73f0e8142fdf8e815c0233b072f781d19897dd22
nabroo101 <elnabawy101@gmail.com>
2023-07-15 23:08:27 -0400

    fixed the bug in infoMap.js and updated readme

--- README.md
+++ README.md
@@ -27,16 +27,31 @@
         - Finding the top ten countries by market capitalization.
         - Querying data specifically for Tesla.
         - Calculating market capitalization using share price and volume information.
+
 - Python to clean and convert csv files to json.
+
 - CSS for styling and we broke it down to 3 PARTS
     - Part 1: This section sets up basic HTML and body settings like setting the height to 100%. It also sets up the #map element's height and width.
     - Part 2: This section includes styles for specific elements or classes.We define the dimensions for the .img-flag class, styles for the .well class, etc.
     - PART 3: This part of your CSS code includes the styles for the .exit-button class which is a button in the map to zoom out to original view.
+
 - HTML for constructing the web page which divided to 3 sections:
     - Header : it includes doctype, metadta and various sylesheets and scripts.
     - Stylesheets: bootstrap for web design ,Leaflet for interactive map and local style. css file
     - Body content : It includes containers for our map, line chart, and bar chart. It also has a drop-down selector and information panel.
     - Scripts: jQuery , and the local javascript file leaflet-timeline-slider.js
+
+- Java Script: we used Jave Script for our web development. Our repo cintains 3 JavaScript files:
+    - automakers.js:
+
+    - infoMap.js: This JavaScript code mainly functions to generate an interactive map using the Leaflet library. It visualizes different car manufacturers around the globe. The map's primary features include customized marker icons based on company rankings, a timeline slider, and an 'exit' button to revert the map to the initial view.
+
+    
+
+
+
+    - leaflet-timeline-slider.js
+
 
 - Plotly for interactive bar and line graphs visualizing stock price changes over time.  
 - Javascript utilizing 'd3', HTML, and CSS to create and display interactive graphs & charts with proper formatting.
--- static/infoMap.js
+++ static/infoMap.js
@@ -1,44 +1,4 @@
-// setting the street layer
-let street = L.tileLayer(
-  "https://basemap.nationalmap.gov/arcgis/rest/services/USGSTopo/MapServer/tile/{z}/{y}/{x}",
-  {
-    maxZoom: 20,
-    attribution:
-      'Tiles courtesy of the <a href="https://usgs.gov/">U.S. Geological Survey</a>' +
-      "<br> Analyst: Mo Abou <a href=https://github.com/nabroo101> Github<a/>",
-  }
-);
-
-let Staellite = L.tileLayer(
-  "https://{s}.tile.opentopomap.org/{z}/{x}/{y}.png",
-  {
-    attribution:
-      'Map data: &copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors' +
-      "<br> Analyst: Mo Abou <a href=https://github.com/nabroo101/leaflet-challenge> Github<a/>",
-  }
-);
-
-let carManufacturers = new L.layerGroup(); // This layer group contains all the car manufacturer icons
-
-// creating a base map object
-let baseMaps = {
-  "Street Map": street,
-  Staellite: Staellite,
-};
-
-let ranking = new L.layerGroup();
-
-//creating overlay object
-let overlayMaps = {
-  Ranking: ranking,
-  "Manufacturers info": carManufacturers,
-};
-//my baseMap, with toggles between info and ranking
-let myMap = L.map("map", {
-  center: [39, 4],
-  zoom: 2,
-  layers: [street, ranking],
-});
+// Calculates the size of the marker icon based on rank , however tesla is set to be biggest
 function getIconSize(rank, symbol) {
   if (symbol === "TSLA") {
     return [100, 100]; // Set a fixed size for Tesla
@@ -49,93 +9,13 @@ function getIconSize(rank, symbol) {
 
   if (rank > 0 && rank <= 10) {
     // Adjust the size for the top 10 companies
-    size = teslaSize * (11 - rank) / 10;
+    size = (teslaSize * (11 - rank)) / 10;
   }
 
   return [size, size];
 }
 
-const largestData = "../Resources/infoMapData.json";
-
-//defininf the timeline variable and setting the parameters for certain years
-function markerSize(magnitude) {
-  return magnitude / 2; // Adjust the division factor as needed
-}
-let timeline = L.control.timelineSlider({
-  timelineItems: ["Before", "After"],
-  changeMap: function ({ label, value, map }) {
-    console.log("Changed time to:", label);
-    d3.json(largestData).then(function (data){
-      let companies = reformatData(data);
-     
-      
-      if (label === "After") {
-        carManufacturers.clearLayers();
-        companies.forEach((company) => {
-          let customIcon = L.icon({
-            iconUrl: company.Logo,
-            iconSize: getIconSize(company.marketcap, company.Symbol),
-            iconAnchor: [19, 19],
-            popupAnchor: [-3, -76],
-          });
-        // assigning the marker to a variable
-        let marker = L.marker([company.Latitude, company.Longitude], {
-          icon: customIcon,
-        });
-        marker
-          .bindPopup(
-            `<h2>${company.Name}</h2>
-<h3>Rank: ${company.Rank}</h3>
-<p>
-  <span class="large-text">Market Cap: ${company.marketcap}</span><br>
-  <span class="large-text">Price (USD): ${company["price (USD)"]}</span><br>
-  Country: ${company.country}
-</p>`
-
-          )
-          .on("click", function (e) {
-            myMap.setView(e.latlng, 8);
-          })
-          .addTo(carManufacturers);
-      });
-    }
-  });
-  },
-})
-
-
-timeline.addTo(myMap);
-
-let originalZoom = myMap.getZoom();
-let originalCenter = myMap.getCenter();
-
-L.control
-  .layers(baseMaps, overlayMaps, {
-    collapsed: false,
-  })
-  .addTo(myMap);
-
-//creating a button element
-let exitButton = L.control({ position: "bottomright" });
-
-exitButton.onAdd = function (myMap) {
-  this._div = L.DomUtil.create("div", "exit-button");
-  this._div.innerHTML = "Exit";
-  this._div.onmousedown = this._div.onclick = function () {
-    myMap.setView(originalCenter, originalZoom);
-  };
-  return this._div;
-};
-// Adding the exit button to the map
-exitButton.addTo(myMap);
-
-// Event listener for the button
-document.querySelector(".exit-button").addEventListener("click", function () {
-  myMap.setView(originalCenter, originalZoom);
-});
-
-
-// converting the json file to a difrrent format
+// Converts the data into a more usable format
 function reformatData(data) {
   let result = [];
   let length = Object.keys(data.Name).length;
@@ -155,7 +35,124 @@ function reformatData(data) {
     };
     result.push(company);
   }
-
   return result;
 }
 
+/* Initialization:  creating two map tile layers (street and Staellite),
+ */
+let street = L.tileLayer(
+  "https://basemap.nationalmap.gov/arcgis/rest/services/USGSTopo/MapServer/tile/{z}/{y}/{x}",
+  {
+    maxZoom: 20,
+    attribution:
+      'Tiles courtesy of the <a href="https://usgs.gov/">U.S. Geological Survey</a>' +
+      "<br> Analyst: Mo Abou <a href=https://github.com/nabroo101> Github<a/>",
+  }
+);
+
+let Staellite = L.tileLayer(
+  "https://{s}.tile.opentopomap.org/{z}/{x}/{y}.png",
+  {
+    attribution:
+      'Map data: &copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors' +
+      "<br> Analyst: Mo Abou <a href=https://github.com/nabroo101/leaflet-challenge> Github<a/>",
+  }
+);
+// layer groups for car manufacturers and rankings, and a base map with toggleable overlays.
+let carManufacturers = new L.layerGroup(); // This layer group contains all the car manufacturer icons
+let ranking = new L.layerGroup();
+
+// creating a base map object
+let baseMaps = {
+  "Street Map": street,
+  Staellite: Staellite,
+};
+
+//creating overlay object
+let overlayMaps = {
+  Ranking: ranking,
+  "Manufacturers info": carManufacturers,
+};
+
+//my baseMap, with toggles between info and ranking
+let myMap = L.map("map", {
+  center: [39, 4],
+  zoom: 2,
+  layers: [street, ranking],
+});
+
+const largestData = "../Resources/infoMapData.json";
+
+//defining the timeline variable and setting the parameters for certain years
+function markerSize(magnitude) {
+  return magnitude / 2; // Adjust the division factor as needed
+}
+let timeline = L.control.timelineSlider({
+  timelineItems: ["Before", "After"], // chooseing what showing on the timeline bar
+  changeMap: function ({ label, value, map }) {
+    console.log("Changed time to:", label);
+    d3.json(largestData).then(function (data) {
+      let companies = reformatData(data);
+
+      if (label === "After") {
+        carManufacturers.clearLayers();
+        companies.forEach((company) => {
+          let customIcon = L.icon({
+            iconUrl: company.Logo,
+            iconSize: getIconSize(company.marketcap, company.Symbol),
+            iconAnchor: [19, 19],
+            popupAnchor: [-3, -76],
+          });
+          // assigning the marker to a variable
+          let marker = L.marker([company.Latitude, company.Longitude], {
+            icon: customIcon,
+          });
+          marker
+            .bindPopup(
+              `<h2>${company.Name}</h2>
+<h3>Rank: ${company.Rank}</h3>
+<p>
+  <span class="large-text">Market Cap: ${company.marketcap}</span><br>
+  <span class="large-text">Price (USD): ${company["price (USD)"]}</span><br>
+  Country: ${company.country}
+</p>`
+            )
+            .on("click", function (e) {
+              myMap.setView(e.latlng, 8);
+            })
+            .addTo(carManufacturers);
+        });
+      }
+    });
+  },
+});
+// Add timeline control to the map
+timeline.addTo(myMap);
+
+let originalZoom = myMap.getZoom();
+let originalCenter = myMap.getCenter();
+// Add layers control to the map
+L.control
+  .layers(baseMaps, overlayMaps, {
+    collapsed: false,
+  })
+  .addTo(myMap);
+
+//creating exit button element
+let exitButton = L.control({ position: "bottomright" });
+
+exitButton.onAdd = function (myMap) {
+  this._div = L.DomUtil.create("div", "exit-button");
+  this._div.innerHTML = "Exit";
+  this._div.onmousedown = this._div.onclick = function () {
+    myMap.setView(originalCenter, originalZoom);
+  };
+  return this._div;
+};
+// Adding the exit button to the map
+exitButton.addTo(myMap);
+
+// Event listener for the button
+document.querySelector(".exit-button").addEventListener("click", function () {
+  myMap.setView(originalCenter, originalZoom);
+});
